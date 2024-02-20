a55d5d4c309821aff461e2f8a38a7c9910d2a8e9
RetrozDev <ryanbarbozapro@gmail.com>
2024-02-20 23:42:18 +0100

    Express 04 - 🗑️ Méthode DELETE et suppression de données

--- src/app.js
+++ src/app.js
@@ -17,12 +17,16 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", movieControllers.postMovie);
 app.put("/api/movies/:id", movieControllers.updateMovie);
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
+
 
 
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 app.post("/api/users", userControllers.postUser);
 app.put("/api/users/:id", userControllers.updateUser);
+app.delete("/api/movies/:id", userControllers.deleteUser);
+
 
 
 
--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -69,9 +69,29 @@ const updateUser = (req, res) => {
     });
 };
 
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+
 module.exports = {
     getUsers,
     getUserById,
     postUser,
     updateUser,
+    deleteUser
 };
